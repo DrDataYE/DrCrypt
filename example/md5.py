0544d8cbf0cdda28b1f2a5bc6d81b2022dfae9fe
Dr Data <135477108+DrDataYE@users.noreply.github.com>
2023-08-22 18:17:37 +0200

    Update md5.py

--- example/md5.py
+++ example/md5.py
@@ -1,8 +1,8 @@
-from drcrypt.hash import MD5
+from drcrypt import MD5
 
 
 data = "Hello, MD5!"
-md5_hash = MD5Hash()
+md5_hash = MD5()
 md5_hash.update(data.encode("utf-8"))
 md5_hash.finalize()
 hashed = md5_hash.hexdigest()
