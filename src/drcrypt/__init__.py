77f1de7f14aff2504bd96e79313449eb63b30376
Dr Data <135477108+DrDataYE@users.noreply.github.com>
2023-08-25 01:52:52 +0200

    Update __init__.py

--- src/drcrypt/__init__.py
+++ src/drcrypt/__init__.py
@@ -1,4 +1,4 @@
-from .crypt import XOR,RSA,encrypt_aes,decrypt_aes
+from .crypt import XOR,RSA,encrypt_aes,decrypt_aes,encrypt_xor,decrypt_xor
 
 from .hash import SHA1,SHA256, MD5, Bcrypt
 from .hash import compare_hash
